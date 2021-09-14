07a113b9d925ee2e8e2af0fd09013add775de973
Your Name <you@example.com>
2021-09-14 20:13:10 +0300

    comit2

--- src/yourdata.js
+++ src/yourdata.js
@@ -20,11 +20,11 @@ export default {
   name: "AlbDesignStudio",
   headerTagline: [
     //Line 1 For Header
-    "Building digital",
+    "Proiecte design",
     //Line 2 For Header
-    "products, brands,",
+    "interior, profesionale",
     //Line 3 For Header
-    "and experience",
+    "la cel mai inalt nivel",
   ],
   //   Header Paragraph
   headerParagraph:
@@ -45,7 +45,7 @@ export default {
       imageSrc:
         "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "http://localhost:8001/src/pages/child_page.js",
     },
     {
       title: "Project Two", //Project Title - Add Your Project Title Here
